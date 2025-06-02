da56c6f2b541acd174e8ba4a1948438b3c207958
Abhishek Paul <abhishekpaul04022006@gmail.com>
2025-06-02 21:55:53 +0530

    Formatted the Output for Cyberpunk Appearance

--- inheadlines.py
+++ inheadlines.py
@@ -5,9 +5,9 @@ import os
 
 scarlet_red="\033[38;2;255;0;60m"
 reset="\033[0m"
-cyber_green="\033[38;2;7;242;129m"
 glitch_violet="\033[38;2;139;0;255m"
 dream_blue="\033[38;2;57;163;250m"
+lime_green="\033[38;2;175;255;51m"
 
 def banner():
     print(scarlet_red+pyfiglet.figlet_format("Intel",font="ansi_shadow")+reset,end="\n")
@@ -20,17 +20,19 @@ def india():
         elements1 = soup.find_all("a", class_="NwsLstPg_ttl-lnk")  
         elements2 = soup.find_all("p",class_="NwsLstPg_txt txt_tct txt_tct-three")
         if elements1 and elements2:
+            counter=0
             for a_tag,p_tag in zip(elements1,elements2):  
+                counter+=1
                 if a_tag:
                     headline=a_tag.get_text(strip=True)
                 if p_tag:
                     titlest=p_tag.get_text(strip=True)
-                print(f"{cyber_green+headline+reset}\n{glitch_violet+titlest+reset}\n")
+                print(f"{scarlet_red+str(counter)+'.'+lime_green+headline+reset}\n{glitch_violet+titlest+reset}\n")
         else:
             print("Website HTML Changed")
     else:
         print("Failed to fetch page. Status code:", r.status_code)
-    print(f"{dream_blue}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
+    print(f"{lime_green}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
     ch=input("")
     
 
@@ -42,9 +44,9 @@ def clear_screen():
 
 def choice():
     banner()
-    print(f"{dream_blue}Select your intel feed, citizen...\n{reset}")
-    print(f"{dream_blue}1. INDIA\n2. EXit\n{reset}")
-    ch=int(input(f"{dream_blue}ENtEr: "))
+    print(f"{lime_green}Select your intel feed, citizen...\n{reset}")
+    print(f"{lime_green}1. INDIA\n2. EXIT\n{reset}")
+    ch=int(input(f"{lime_green}ENTER: "))
     print(reset)
     if(ch==1):
         clear_screen()
