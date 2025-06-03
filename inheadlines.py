63a42cbaf83048a6ac780b5640d0814a7f03131a
Abhishek Paul <abhishekpaul04022006@gmail.com>
2025-06-03 20:48:22 +0530

    Added more sections and formatted the output

--- inheadlines.py
+++ inheadlines.py
@@ -6,17 +6,19 @@ import os
 scarlet_red="\033[38;2;255;0;60m"
 reset="\033[0m"
 glitch_violet="\033[38;2;139;0;255m"
-dream_blue="\033[38;2;57;163;250m"
+dream_blue="\033[38;2;2;207;247m"
 lime_green="\033[38;2;175;255;51m"
 
-url_list=["https://www.ndtv.com/india#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/india-global#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/asia#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/europe#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/australia#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/americas#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/africa#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/us#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/uk#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/middle-east#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/science#pfrom=home-ndtv_mainnavigation","https://www.ndtv.com/world/diaspora#pfrom=home-ndtv_mainnavigation"]
+domain="https://www.ndtv.com/"
+url_list=["india","india-global","world/asia","world/europe","world/australia","world/americas","world/africa","world/us","world/uk","world/middle-east","science","world/diaspora","environment#pfrom=home-nav"]
+suffix="#pfrom=home-ndtv_mainnavigation"
 
 def banner():
     print(scarlet_red+pyfiglet.figlet_format("Intel",font="ansi_shadow")+reset,end="\n")
 
 def intel(url):
     banner()
-    r = requests.get(url)
+    r = requests.get(domain+url+suffix)
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, 'html.parser')
         elements1 = soup.find_all("a", class_="NwsLstPg_ttl-lnk")  
@@ -31,10 +33,54 @@ def intel(url):
                     titlest=p_tag.get_text(strip=True)
                 print(f"{scarlet_red+str(counter)+'.'+lime_green+headline+reset}\n{glitch_violet+titlest+reset}\n")
         else:
-            print("Website HTML Changed")
+            print(f"{scarlet_red}Webframe compromised — DOM mutation detected{reset}")
     else:
-        print("Failed to fetch page. Status code:", r.status_code)
-    print(f"{lime_green}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
+        print(f"{scarlet_red}>> ERROR. STATUS CODE: ", r.status_code,reset)
+    print(f"{scarlet_red}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
+    ch=input("")
+
+def environment(url):
+    banner()
+    r=requests.get(domain+url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        elements = soup.find_all("a", class_="NwsLstPg_ttl-lnk")
+        if elements:
+            counter=0
+            for a_tag in elements:
+                counter+=1
+                if a_tag:
+                    headline=a_tag.get_text(strip=True)
+                    print(f"{scarlet_red+str(counter)}.{dream_blue+headline}\n{reset}")
+        else:
+            print(f"{scarlet_red}Webframe compromised — DOM mutation detected{reset}")
+    else:
+        print(f"{scarlet_red}>> ERROR. STATUS CODE: ", r.status_code,reset)
+    print(f"{scarlet_red}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
+    ch=input("")
+
+def indian_cities():
+    banner()
+    city=input(f"{scarlet_red}>> INITIATE TRACE… Enter city name: {lime_green}").lower()+"-news"
+    clear_screen()
+    banner()
+    url=domain+city+suffix
+    r=requests.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        elements = soup.find_all("a", class_="NwsLstPg_ttl-lnk")
+        if elements:
+            counter=0
+            for a_tag in elements:
+                counter+=1
+                if a_tag:
+                    headline=a_tag.get_text(strip=True)
+                    print(f"{scarlet_red+str(counter)}.{dream_blue+headline}\n{reset}")
+        else:
+            print(f"{scarlet_red}Webframe compromised — DOM mutation detected{reset}")
+    else:
+        print(f"{scarlet_red}>> ERROR. STATUS CODE: ", r.status_code,reset)
+    print(f"{scarlet_red}EXIT POINT IDENTIFIED\nPress ENTER to exit{reset}")
     ch=input("")
     
 
@@ -47,7 +93,7 @@ def clear_screen():
 def choice():
     banner()
     print(f"{lime_green}Select your intel feed, citizen...\n{reset}")
-    print(f"{lime_green}1.  INDIA\n2.  INDIA GLOBAL\n3.  ASIA\n4.  EUROPE\n5.  AUSTRALIA\n6.  AMERICAS\n7.  AFRICA\n8.  US\n9.  UK\n10. MIDDLE EAST\n11. SCIENCE\n12. DIASPORA\n13. EXIT\n{reset}")
+    print(f"{lime_green}1.  INDIA\n2.  INDIA GLOBAL\n3.  ASIA\n4.  EUROPE\n5.  AUSTRALIA\n6.  AMERICAS\n7.  AFRICA\n8.  US\n9.  UK\n10. MIDDLE EAST\n11. SCIENCE\n12. DIASPORA\n13. ENVIRONMENT\n14. INDIAN CITIES\n15. EXIT\n{reset}")
     ch=int(input(f"{lime_green}ENTER: "))
     print(reset)
     if(ch==1): #India
@@ -86,7 +132,13 @@ def choice():
     if(ch==12): #Diaspora
         clear_screen()
         intel(url_list[11])
-    if(ch==13): #Exit
+    if(ch==13): #Environment
+        clear_screen()
+        environment(url_list[12])
+    if(ch==14): #Indian Cities
+        clear_screen()
+        indian_cities()
+    if(ch==15): #Exit
         print(reset)
         clear_screen()
     else:
